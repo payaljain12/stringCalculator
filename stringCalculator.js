ce1ff7be76032675162779e507b864d4f3d39acf
payal <payal12599@gmail.com>
2025-03-22 13:27:58 +0530

    special char handling with formating code

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,15 +1,20 @@
 function add(inputString) {
+    console.log("Input is --- ", inputString);
     if (typeof inputString !== "string") {
         return "Invalid input provided"
     }
     let sum = 0;
-    let numbers =  inputString.split(",");
-    console.log(numbers);
-    for (let index = 0; index < numbers.length; index++) {
+    let numbers = inputString.match(/[-+]?\d*\.?\d+/g)
+    // console.log(numbers, inputString);
+    for (let index = 0; index < numbers?.length; index++) {
         const val = parseInt(numbers[index]);
         sum +=  val
     }
     return sum;
 }
 
-console.log( add("2,3"));
+console.log("Output is -- ", add(0));
+console.log("Output is -- ", add(""));
+console.log("Output is -- ", add("2"));
+console.log("Output is -- ", add("2,3"));
+console.log("Output is -- ", add("2\n5;;;;///"));
