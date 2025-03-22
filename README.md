760114fa2321abc88585bfabaacfc7597fc73468
payal <payal12599@gmail.com>
2025-03-22 14:14:13 +0530

    Negative number handling with comments and test cases

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # stringCalculator
 Adds string numbers and returns output
+
+# How to run the code
+node stringCalculator.js
+
+# Prerequisties
+node dependencies
--- stringCalculator.js
+++ stringCalculator.js
@@ -1,20 +1,33 @@
+// Function to add numbers
 function add(inputString) {
-    console.log("Input is --- ", inputString);
-    if (typeof inputString !== "string") {
-        return "Invalid input provided"
+  console.log("Input is --- ", inputString);
+  // If input type is not string return
+  if (typeof inputString !== "string") {
+    return "Invalid input provided";
+  }
+  let sum = 0;
+  let numbers = inputString.match(/[-+]?\d*\.?\d+/g); // Regex to extract only numbers from the input
+  let negativeNumbers = []; // Holds negative numbers if any
+  for (let index = 0; index < numbers?.length; index++) {
+    const val = parseInt(numbers[index]);
+    if (val < 0) {
+      negativeNumbers.push(val);
+    } else if (negativeNumbers.length == 0) {
+      sum += val;
     }
-    let sum = 0;
-    let numbers = inputString.match(/[-+]?\d*\.?\d+/g)
-    // console.log(numbers, inputString);
-    for (let index = 0; index < numbers?.length; index++) {
-        const val = parseInt(numbers[index]);
-        sum +=  val
-    }
-    return sum;
+  }
+  // Return negative numbers list if present
+  if (negativeNumbers.length) {
+    return `Negative numbers not allowed ${negativeNumbers}`
+  }
+  return sum;
 }
 
+// Test cases
 console.log("Output is -- ", add(0));
 console.log("Output is -- ", add(""));
 console.log("Output is -- ", add("2"));
 console.log("Output is -- ", add("2,3"));
-console.log("Output is -- ", add("2\n5;;;;///"));
+console.log("Output is -- ", add("//;\n1;2"));
+console.log("Output is -- ", add("-2,3"));
+console.log("Output is -- ", add("-2,3, -5,9"));
